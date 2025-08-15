1b526a32d3a46bb67e5ccda13cc474c70740e1c6
PabloContreras14 <jcontreraslondono@gmail.com>
2025-08-15 14:27:57 -0500

    Actualizacion de tareas, agregue el html, css y js de agregar tarea

---
+++ js/agregarResgitros.js
@@ -0,0 +1,25 @@
+const cerrarMenuRegistros = document.getElementById('cerrarMenuRegistros');
+const abrirMenuRegistro = document.getElementById('abrirMenuRegistro');
+const agregarRegistro = document.getElementById('agregarRegistro');
+const containerRegistro = document.getElementById('containerRegistro');
+
+const abrirAsociarGasto = document.getElementById('abrirAsociarGasto');
+const asociarGasto = document.getElementById('asociarGasto');
+
+abrirMenuRegistro.addEventListener('click', () => {
+    agregarRegistro.style.display = 'flex';
+});
+
+cerrarMenuRegistros.addEventListener('click', () => {
+    agregarRegistro.style.display = 'none';
+});
+
+abrirAsociarGasto.addEventListener('click', () => {
+    if (abrirAsociarGasto.checked) {
+        asociarGasto.style.display = 'flex';
+        containerRegistro.style.height = 'auto';
+    } else {
+        asociarGasto.style.display = 'none';
+        containerRegistro.style.height = '500px';
+    }
+});
