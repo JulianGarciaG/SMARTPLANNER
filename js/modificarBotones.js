73e5245bc125cef35d9bf50f60a133ce04d62818
juanjosevg12 <juanjosevergaragraciano@gmail.com>
2025-08-23 18:50:07 -0500

    se modifico el apartado de agregar calendario, se agrego un nuevo archivo js y css para el mismo se modifico la parte de mis calendarios

--- js/modificarBotones.js
+++ js/modificarBotones.js
@@ -6,6 +6,18 @@ document.querySelectorAll(".toggle-ocultar").forEach(boton => {
 
 document.querySelectorAll(".toggle-compartir").forEach(boton => {
   boton.addEventListener("click", () => {
-    boton.textContent = boton.textContent === "Personal" ? "Compartirdo" : "Personal";
+    boton.textContent = boton.textContent === "Personal" ? "Compartir" : "Personal";
+  });
+});
+
+document.querySelectorAll(".toggle-compartir").forEach(btn => {
+  btn.addEventListener("click", () => {
+    const contenedor = btn.closest(".elep-calendario");
+    const badge = contenedor.querySelector(".badge-compartido");
+     if (badge.style.display === "inline-block") {
+      badge.style.display = "none";   // lo oculta
+    } else {
+      badge.style.display = "inline-block";  // lo muestra
+    }
   });
 });
