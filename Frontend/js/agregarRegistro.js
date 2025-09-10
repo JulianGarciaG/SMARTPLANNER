4007b5fb62c4800dc760927f6dab41f18953bef3
PabloContreras14 <jcontreraslondono@gmail.com>
2025-09-10 04:36:30 -0500

    Correccion de errores sobre el despliegue de asociar registro y de diseño intuitivo

--- Frontend/js/agregarRegistro.js
+++ Frontend/js/agregarRegistro.js
@@ -2,7 +2,7 @@ const cerrarMenuRegistros = document.getElementById('cerrarMenuRegistros');
 const abrirMenuRegistro = document.getElementById('abrirMenuRegistro');
 const agregarRegistro = document.getElementById('agregarRegistro');
 const containerRegistro = document.getElementById('containerRegistro');
-const cancelarRegistro = document.getElementById('cancelarRegistro');
+const cancelarRegistro = document.getElementById('Cancelar-registro');
 
 const abrirAsociarGasto = document.getElementById('abrirAsociarGasto');
 const asociarGasto = document.getElementById('asociarGasto');
@@ -15,11 +15,12 @@ cerrarMenuRegistros.addEventListener('click', () => {
     agregarRegistro.style.display = 'none';
 });
 
-cancelarRegistro.addEventListener('click', () => {
+cancelarRegistro.addEventListener('click', (e) => {
+    e.preventDefault();
     agregarRegistro.style.display = 'none';
 });
 
-abrirAsociarGasto.addEventListener('click', () => {
+abrirAsociarGasto.addEventListener('change', () => {
     if (abrirAsociarGasto.checked) {
         asociarGasto.style.display = 'flex';
         containerRegistro.style.height = 'auto';
